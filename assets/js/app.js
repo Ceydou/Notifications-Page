d0cabbe4c8211da6fd41135a1c1f5f537047feaa
Ceydou <cedric.rouedou@gmail.com>
2022-11-30 13:46:49 +0100

    Add app.js and lastest CSS styles

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+const newItem = document.querySelectorAll('.item--new');
+const allRead = document.querySelector('nav a');
+const countNew = document.querySelector('nav h3 span');
+
+for(i = 0; i < newItem.length; i++) {
+    const allNewItems = newItem[i];
+    countNew.textContent = newItem.length;
+    document.title = newItem.length + ' new notifications'
+
+    allRead.addEventListener('click', () => {
+        allRead.classList.add('dnone');
+        allNewItems.classList.remove('item--new');
+        countNew.textContent = 0;
+        document.title = "Pas de nouvelles notifications";
+    })
+}
